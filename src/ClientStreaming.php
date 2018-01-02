aeda90009f36afb22e8229e100486d4f02b19800
Valerijs Krizevics <vkrizevics@gmail.com>
2018-01-03 01:01:27 +0200

    Draft integration with event-driven parsers emitting their events to outer subscribers.

--- src/ClientStreaming.php
+++ src/ClientStreaming.php
@@ -7,17 +7,20 @@ use Clue\React\Soap\Client;
 use Psr\Http\Message\ResponseInterface;
 use React\Promise\Stream;
 
-class ClientStreaming extends Client
+class ClientStreaming extends Client implements EventEmitterInterface
 {
+    use EventEmitterTrait;
 
-    public function __construct($wsdl, Browser $browser, ClientEncoder $encoder = null, ClientDecoder $decoder = null)
+    public function __construct($wsdl, Browser $browser, ClientEncoder $encoder = null, ClientDecoder $decoder = null, ParsersPipelinesEventDriven $parsers_pipelines = null)
     {
         parent::__construct($wsdl, $browser, $encoder, $decoder);
+        $this->parsers_pipelines = !$parsers_pipelines ? ParsersPipelinesEventDriven::getInstance(($this, 'emit')) : $parsers_pipelines;
     }
 
     public function soapCall($name, $args)
     {
-        return Stream\unwrapReadable(parent::soapCall($name, $args));
+        $this->stream = Stream\unwrapReadable(parent::soapCall($name, $args))->on('data', ($this, 'emit'))->on('data',($this->parsers_pipelines, 'applyParsers'))->on('error', ($this, 'emit'))->on('end', ($this, 'emit'));
+         return $this;
     }
 
     public function handleResponse(ResponseInterface $response)
